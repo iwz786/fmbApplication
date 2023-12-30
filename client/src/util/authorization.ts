cb39f8498d3718b3b649cfa37c165aa2a985779a
Hussain Mansoor <hussain_raj1@yahoo.com>
2023-12-30 18:17:21 -0500

    Optimises the code

--- client/src/util/authorization.ts
+++ client/src/util/authorization.ts
@@ -3,25 +3,10 @@ export const FMB_ROLE_OPERATIONS = 'FMB_ROLE_OPERATIONS';
 export const FMB_ROLE_SUBSCRIBER = 'FMB_ROLE_SUBSCRIBER';
 export const FMB_ROLE_CRM_OPERATION = 'FMB_ROLE_CRM_OPERATION';
 
-export const doesUserBelongsToOperation = (roles: string[]): boolean => {
-  return (
-    roles && roles.filter(role => role === FMB_ROLE_OPERATIONS).length === 1
-  );
-};
+export const doesUserBelongsToOperation = (roles: string[]): boolean => roles?.includes(FMB_ROLE_OPERATIONS) ?? false;
 
-export const doesUserBelongsToCRMOperation = (roles: string[]): boolean => {
-  return (
-    roles && roles.filter(role => role === FMB_ROLE_CRM_OPERATION).length === 1
-  );
-};
+export const doesUserBelongsToCRMOperation = (roles: string[]): boolean => roles?.includes(FMB_ROLE_CRM_OPERATION) ?? false;
 
+export const isUserSubscriber = (roles: string[]): boolean => roles?.includes(FMB_ROLE_SUBSCRIBER) ?? false;
 
-export const isUserSubscriber = (roles: string[]): boolean => {
-  return (
-    roles && roles.filter(role => role === FMB_ROLE_SUBSCRIBER).length === 1
-  );
-};
-
-export const isSuper = (roles: string[]): boolean => {
-  return roles && roles.filter(role => role === FMB_ROLE_SUPER).length === 1;
-};
+export const isSuper = (roles: string[]): boolean => roles?.includes(FMB_ROLE_SUPER) ?? false;
